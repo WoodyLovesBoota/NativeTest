aa2f2a7787c55ab2c43f509db6c147c29b7f5dcc
woody <woodylovesboota@gmail.com>
2024-02-05 22:24:32 +0900

    날씨 받아오기

--- weather-app/App.tsx
+++ weather-app/App.tsx
@@ -1,15 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Dimensions, ActivityIndicator } from "react-native";
 import * as Location from "expo-location";
+import { IWeatherData } from "./api";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
+const WEATHER_API_KEY = "f8c4be74703179b86960ad34cc953c76";
 
 export default function App() {
-  const [location, setLocation] = useState();
   const [ok, setOk] = useState(true);
   const [city, setCity] = useState<string | null>("loading...");
+  const [location, setLocation] = useState<{ latitude: number; longitude: number } | undefined>();
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState<IWeatherData>();
 
   const ask = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
@@ -17,14 +21,32 @@ export default function App() {
     const {
       coords: { latitude, longitude },
     } = await Location.getCurrentPositionAsync();
+    setLocation({ latitude, longitude });
     const location = await Location.reverseGeocodeAsync({ latitude, longitude }, { useGoogleMaps: false });
-    console.log(location[0].city);
     setCity(location[0].city);
+  };
+
+  const getWeather = async (loc: { latitude: number; longitude: number }) => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${loc.latitude}&lon=${loc.longitude}&APPID=${WEATHER_API_KEY}`
+      );
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
     ask();
   }, []);
+
+  useEffect(() => {
+    location && getWeather(location);
+  }, [location]);
 
   return (
     <View style={styles.container}>
@@ -36,10 +58,18 @@ export default function App() {
           <Text style={styles.temp}>27</Text>
           <Text style={styles.weatherName}>sunny</Text>
         </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.weatherName}>sunny</Text>
-        </View>
+        {data ? (
+          <View style={styles.day}>
+            <Text style={styles.temp}>{(data.main.temp - 273.15).toFixed(1)}</Text>
+            <Text style={styles.weatherName}>{data.weather[0].main}</Text>
+            <Text style={styles.weatherName}>{data.weather[0].description}</Text>
+          </View>
+        ) : (
+          <View style={styles.day}>
+            <ActivityIndicator color={"black"} size={20} />
+          </View>
+        )}
+
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.weatherName}>sunny</Text>
